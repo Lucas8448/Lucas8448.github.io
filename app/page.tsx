4865b8d540574a3d0d7516b6ea299dedbe729aa8
Lucas Bateson <lucas.bateson@gmail.com>
2024-01-25 21:31:31 +0100

    Setup

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function IndexPage() {
+  return (
+    <section className="container grid items-center gap-6 pt-6 pb-8 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Hello!
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
+          My name is Lucas, im a student studying computer science.
+        </p>
+      </div>
+    </section>
+  )
+}
---
+++ config/site.ts
@@ -0,0 +1,24 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Lucas Bateson",
+  description:
+    "Private showcase of my skills and portfolio",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Projects",
+      href: "/projects",
+    },
+    {
+      title: "Contact",
+      href: "/contact",
+    },
+  ],
+  links: {
+    github: "https://github.com/Lucas8448",
+  },
+}
